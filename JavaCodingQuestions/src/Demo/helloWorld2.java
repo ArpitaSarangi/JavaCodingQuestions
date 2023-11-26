fb8ad271eb600d084f6fce336dfddd8956435cfa
Arpita Sarangi <arpitasarangi97@gmail.com>
2023-11-26 14:02:12 +0530

    Update helloWorld2.java

--- JavaCodingQuestions/src/Demo/helloWorld2.java
+++ JavaCodingQuestions/src/Demo/helloWorld2.java
@@ -5,6 +5,7 @@ public class helloWorld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World2");
+		System.out.println("Hello World1");
 	}
 
 }
