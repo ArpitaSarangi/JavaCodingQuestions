15e0723fef72bafcc071c7ba9c3ebb651dc53adb
Arpita Sarangi <arpitasarangi97@gmail.com>
2023-11-30 00:13:21 +0530

    Palindrome Number

--- JavaCodingQuestions/src/NumberPrograms/PalindromeNumber.java
+++ JavaCodingQuestions/src/NumberPrograms/PalindromeNumber.java
@@ -1,9 +1,24 @@
 package NumberPrograms;
 
+import java.util.Scanner;
+
 public class PalindromeNumber {
 
 	public static void main(String[] args) {
-		System.out.println("Palindrome Number");
+		System.out.println("Enter a number to check Palindrome or not.");
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
+		int sameNumber=number;
+		int reversedNumber=0;		
+		while(sameNumber>0) {
+			reversedNumber=sameNumber%10 + reversedNumber*10;
+			sameNumber=sameNumber/10;
+		}		
+		if(number==reversedNumber) {
+			System.out.println("Palindrome Number");
+		}else {
+			System.out.println("Not Palindrome Number");
+		}
 
 	}
 
