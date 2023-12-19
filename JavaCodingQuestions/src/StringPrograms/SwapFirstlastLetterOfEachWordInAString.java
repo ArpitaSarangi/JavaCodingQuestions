22b603d99ad9b207f2046f777b1bce0e30eaae98
Arpita Sarangi <arpitasarangi97@gmail.com>
2023-12-19 21:03:23 +0530

    String programs

---
+++ JavaCodingQuestions/src/StringPrograms/SwapFirstlastLetterOfEachWordInAString.java
@@ -0,0 +1,35 @@
+package StringPrograms;
+
+public class SwapFirstlastLetterOfEachWordInAString {
+
+	public static void main(String[] args) {
+
+		// This is a good day , Thank you
+
+		// shiT si a doog yad , khanT uoy
+		String old = "This is a good day , Thank you";
+
+		String[] arr = null;
+
+		for (int i = 0; i < old.length(); i++) {
+			arr = old.split(" ");
+			char[] c = new char[arr[i].length()];
+			for (int j = 0; j < arr[i].length(); j++) {
+
+				if (j == 0 || j == arr[i].length() - 1) {
+					c[j] = arr[i].charAt(arr[i].length() - 1 - j);
+				} else {
+					c[j] = arr[i].charAt(j);
+				}
+
+				// System.out.print(c[j]);
+			}
+
+			String newValue = new String(c);
+			System.out.println(c);
+
+		}
+
+	}
+
+}
