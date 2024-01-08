53be50e8d205e545d46d98133f72981d25e30085
Arpita Sarangi <arpitasarangi97@gmail.com>
2024-01-08 23:57:03 +0530

    Array List reversal

--- JavaCodingQuestions/src/CollectionPrograms/ReverseArrayList.java
+++ JavaCodingQuestions/src/CollectionPrograms/ReverseArrayList.java
@@ -1,10 +1,28 @@
 package CollectionPrograms;
 
+import java.util.ArrayList;
+
 public class ReverseArrayList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<String> al=new ArrayList<>();
+		al.add("1");
+		al.add("11");
+		al.add("10");
+		al.add("12");
+		al.add("13");
+		al.add("14");
+		al.add("15");
+		al.add("16");
+		al.add("17");
+		al.add("18");
+		System.out.println(al);
+		ArrayList<String> al2=new ArrayList<>(al.size());
+		
+		for(int i=al.size()-1;i>=0;i--) {
+			
+		}
+		
 	}
 
 }
