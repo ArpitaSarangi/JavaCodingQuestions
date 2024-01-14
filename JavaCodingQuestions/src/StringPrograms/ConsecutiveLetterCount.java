7f3c9c3faf323772ed0b3bb09de86b48ea7b9aa8
Arpita Sarangi <arpitasarangi97@gmail.com>
2024-01-14 14:11:15 +0530

    Cosecutive Letter Count using for loop

--- JavaCodingQuestions/src/StringPrograms/ConsecutiveLetterCount.java
+++ JavaCodingQuestions/src/StringPrograms/ConsecutiveLetterCount.java
@@ -16,6 +16,26 @@ public class ConsecutiveLetterCount {
 			}
 		}
 
+		
+		/*
+		 * 
+		String s="aabbbcccc";
+		int count=1;
+		
+
+				//Ouput:2a3b4c
+		//ArrayList<String> a=new ArrayList<String>(s.length());
+				
+				for(int i=0;i<s.length();i++) {
+					if((i!=s.length()-1) && s.charAt(i)==(s.charAt(i+1))) {
+						count++;
+					}else {
+					System.out.print(count+""+s.charAt(i));
+					count=1;
+					}
+				}
+			
+		 */
 	}
 
 }
