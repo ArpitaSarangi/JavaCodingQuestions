fe2c88cbef0377ce84a21b08e6d361ae1f4ec2c6
Arpita Sarangi <arpitasarangi97@gmail.com>
2024-01-29 13:44:35 +0530

    Code improved

--- JavaCodingQuestions/src/StringPrograms/PalindromeString.java
+++ JavaCodingQuestions/src/StringPrograms/PalindromeString.java
@@ -3,34 +3,25 @@ package StringPrograms;
 import java.util.Scanner;
 
 public class PalindromeString {
+	public boolean checkPalindrome(String original) {
+		String reverse = "";
+		for (int i = 0; i <= original.length() - 1; i++) {
+			reverse += original.charAt(original.length() - 1 - i);
+		}
+		System.out.println("Reverse String:" + reverse);
+		if (original.equalsIgnoreCase(reverse)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 
 	public static void main(String[] args) {
-	System.out.println("Enter the String to check Palindrome or not:");	
-	Scanner sc=new Scanner(System.in);
-	String original=sc.nextLine();
-	System.out.println("Original String:"+ original);
-	
-	//Converting String to Character Array
-	char[] originalArray=original.toCharArray();
-	
-	//Reverse character Array
-	char[] reverse=new char[original.length()];
-	for(int i=0;i<=originalArray.length-1;i++) 
-	{	
-		reverse[i]=originalArray[originalArray.length-i-1];
+		System.out.println("Enter the String to check Palindrome or not:");
+		Scanner sc = new Scanner(System.in);
+		String original = sc.nextLine();
+		PalindromeString p = new PalindromeString();
+		System.out.println(p.checkPalindrome(original));
 	}
-	
-	//Converting Character array to String
-	String reverseString=new String(reverse);
-	System.out.println("Reverse String:" + reverseString);
-	
-	//Compare Original string with Reversed String
-	if(original.equals(reverseString)) {
-		System.out.println("String is Palidrome.");
-	}else {
-		System.out.println("String is not Palindrome.");
-	}
-	
-	}
-
 }
