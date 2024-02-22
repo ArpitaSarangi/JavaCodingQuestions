708a2905f056a589500734fa8dba1b017a2a11b2
Arpita Sarangi <arpitasarangi97@gmail.com>
2024-02-22 19:09:36 +0530

    array to string conversion

--- JavaCodingQuestions/src/CollectionTypeChange/StringArrayToString.java
+++ JavaCodingQuestions/src/CollectionTypeChange/StringArrayToString.java
@@ -1,11 +1,13 @@
 package CollectionTypeChange;
 
+import java.util.Arrays;
+
 public class StringArrayToString {
 
 	public static void main(String[] args) {
 		String[] s= {"Mama","is","a","good","girl"};
 		
-		String p=s.toString();
+		String p=Arrays.toString(s);
 		
 		System.out.println(p);
 
