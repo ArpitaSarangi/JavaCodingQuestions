d71292643af7d018d1ac29c94a5a1ea275e08cfa
arpitasarangi <arpitasarangi97@gmail.com>
2025-06-21 12:59:41 +0530

    updated & optimized code

--- JavaCodingQuestions/src/StringPrograms/ReverseString.java
+++ JavaCodingQuestions/src/StringPrograms/ReverseString.java
@@ -1,16 +1,24 @@
 package StringPrograms;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
 	public static void main(String[] args) {
+		
 				//your code goes here
 				//reverse string: ereh seog edoc ruoy
-				String org="your code goes here";
+		
+				System.out.println("Enter your string:");
+				Scanner sc=new Scanner (System.in);
+				String org=sc.nextLine();
 				String revString=" ";
+				
 				for(int i=org.length()-1;i>=0;i--){
 				    revString+=org.charAt(i);
 				}
-				System.out.println("Message :"+revString);
+				
+				System.out.println("Reversed String :"+revString);
 
 	}
 
