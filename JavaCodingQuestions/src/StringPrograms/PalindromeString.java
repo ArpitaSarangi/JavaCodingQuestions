29a3acac17077bc60bada5831c534affe042da4a
arpitasarangi <arpitasarangi97@gmail.com>
2025-06-21 15:52:12 +0530

    Optimized code

--- JavaCodingQuestions/src/StringPrograms/PalindromeString.java
+++ JavaCodingQuestions/src/StringPrograms/PalindromeString.java
@@ -1,13 +1,18 @@
 package StringPrograms;
 
+import java.util.Scanner;
 
 public class PalindromeString {
 	public boolean checkPalindrome(String original) {
+
 		String reverse = "";
-		for (int i = 0; i <= original.length() - 1; i++) {
-			reverse += original.charAt(original.length() - 1 - i);
+
+		for(int i=original.length()-1;i>=0;i--) {
+			reverse+=original.charAt(i);
 		}
-		System.out.println("Reverse String:" + reverse);
+
+
+		System.out.println("Reverse String : " + reverse);
 		if (original.equalsIgnoreCase(reverse)) {
 			return true;
 		} else {
@@ -17,7 +22,9 @@ public class PalindromeString {
 	}
 
 	public static void main(String[] args) {
-		String original = "madam";
+		System.out.println("Please enter your string: ");
+		Scanner sc=new Scanner(System.in);
+		String original = sc.nextLine();
 		PalindromeString p = new PalindromeString();
 		System.out.println(p.checkPalindrome(original));
 	}
