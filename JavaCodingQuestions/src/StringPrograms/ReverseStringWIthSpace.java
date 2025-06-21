06d0cb519282630c4b9a2603fd09b5ea85c90e8e
arpitasarangi <arpitasarangi97@gmail.com>
2025-06-21 13:21:59 +0530

    optimized code

--- JavaCodingQuestions/src/StringPrograms/ReverseStringWIthSpace.java
+++ JavaCodingQuestions/src/StringPrograms/ReverseStringWIthSpace.java
@@ -7,18 +7,22 @@ public class ReverseStringWIthSpace {
 	public static void main(String[] args) {
 		
 		//String old="Interview";
-		
+		//ReverseStringWIthSpace:w e i v r e t n I
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter any string");
 		String old=sc.nextLine();
 
 		String reverseString[] = new String[old.length()];
-		for(int i=0;i<old.length();i++) {		
-
-			reverseString[i]=old.charAt((old.length())-i-1)+" ";
+		
+//		for(int i=0;i<old.length();i++) {		
+//			reverseString[i]=old.charAt((old.length())-i-1)+" ";
+//			System.out.print(reverseString[i]);
+//		}
+		
+		for(int i=old.length()-1;i>=0;i--) {
+			reverseString[i]=old.charAt(i)+" ";
 			System.out.print(reverseString[i]);
 		}
-
 
 	}
 
