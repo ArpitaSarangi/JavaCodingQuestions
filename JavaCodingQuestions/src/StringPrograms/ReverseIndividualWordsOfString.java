2864d2940403565b1208a7fa2a22ef777558ec7a
arpitasarangi <arpitasarangi97@gmail.com>
2025-06-25 14:15:35 +0530

    Each word reverse in a sentence

--- JavaCodingQuestions/src/StringPrograms/ReverseIndividualWordsOfString.java
+++ JavaCodingQuestions/src/StringPrograms/ReverseIndividualWordsOfString.java
@@ -1,10 +1,34 @@
 package StringPrograms;
 
+import java.util.Scanner;
+
 public class ReverseIndividualWordsOfString {
 
 	public static void main(String[] args) {
-		
 
+		//		Hello World
+		//		Reverse Individual Words:olleH DlroW
+		System.out.println("Enter any String:");
+		Scanner sc=new Scanner(System.in);
+		String org=sc.nextLine();
+		
+			String word[]=org.split(" ");
+			
+			for(int i=0;i<=word.length-1;i++) {
+
+			char[] reverseWord=word[i].toCharArray();
+
+			if(i!=word.length-1) {
+			for(int j=reverseWord.length-1;j>=0;j--) {
+				System.out.print(reverseWord[j]);	
+			}
+			System.out.print(" ");
+			}else {
+				for(int j=reverseWord.length-1;j>=0;j--) {
+					System.out.print(reverseWord[j]);	
+				}
+			}
+		}
 	}
 
 }
