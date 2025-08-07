6d3f7e178fc83c81c4845338e75807705efe6ede
arpitasarangi <arpitasarangi97@gmail.com>
2025-08-07 14:10:10 +0530

    List & Set Practiced

--- JavaCodingQuestions/src/ListPrograms/Test.java
+++ JavaCodingQuestions/src/ListPrograms/Test.java
@@ -9,28 +9,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
-	
-	
+
 	public static void main(String[] args) {
-		
-		WebDriver driver ;
-		
-		driver=new ChromeDriver();
-		
+
+		WebDriver driver;
+
+		driver = new ChromeDriver();
+
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
-		
-		List<WebElement> list=driver.findElements(By.tagName("div"));
-				int count=0;
-	System.out.println(list.size());
-		for(WebElement s:list) {
-			String m=s.getText();
-			if(!m.isEmpty()) {
+
+		List<WebElement> list = driver.findElements(By.tagName("div"));
+		int count = 0;
+		System.out.println(list.size());
+		for (WebElement s : list) {
+			String m = s.getText();
+			if (!m.isEmpty()) {
 				System.out.println(m);
 				count++;
-			}					
+			}
 		}
-		System.out.println("Total links: "+count);
+		System.out.println("Total links: " + count);
 		driver.quit();
 	}
 
--- JavaCodingQuestions/src/SetPrograms/TestSet.java
+++ JavaCodingQuestions/src/SetPrograms/TestSet.java
@@ -1,7 +1,13 @@
 package SetPrograms;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -10,23 +16,54 @@ public class TestSet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Set<String> fruts=new TreeSet<String>();
-		
-		
-		fruts.add("Apple");
-		fruts.add("Mango");
-		fruts.add("Banana");
-		fruts.add("Orange");
-		fruts.add("Apple2");
-		fruts.add("Banana2");
-		fruts.add("Banana2");
-		fruts.add("Orange2");
-		//fruts.clear();
-		
-		System.out.println(fruts.contains("Orange2"));
-		
-		System.out.println(fruts);
+//		Set<String> fruts=new TreeSet<String>();
+//		
+//		
+//		fruts.add("Apple");
+//		fruts.add("Mango");
+//		fruts.add("Banana");
+//		fruts.add("Orange");
+//		fruts.add("Apple2");
+//		fruts.add("Banana2");
+//		fruts.add("Banana2");
+//		fruts.add("Orange2");
+//		//fruts.clear();
+//		
+//		System.out.println(fruts.contains("Orange2"));
+//		
+//		System.out.println(fruts);
 
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.amazon.com");
+		
+		List<WebElement> elements=driver.findElements(By.tagName("a"));
+		
+		
+		System.out.println("Total a tags count :"+elements.size());
+		
+		Set<String> tagNames=new HashSet<>();
+		
+		for(WebElement tag:elements) {
+			String tagName=tag.getText();
+			if(!tagName.isEmpty()) {
+				System.out.println(tagName);
+				tagNames.add(tagName);
+			}
+		}
+	
+		System.out.println("*************************************************************");
+		System.out.println("Total unique a tags count :"+tagNames.size());
+	
+			
+		for(String tagName:tagNames) {
+			System.out.println(tagName);
+		}
+		
+		driver.quit();
+		
+		
 	}
 
 }
